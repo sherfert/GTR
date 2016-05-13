1f0e315a4fa694bb84b20c42e828ad2222e02670
Satia Herfert <satiaherfert@gmx.de>
2016-05-13 11:29:48 +0200

    Line based ddmin, as a comparison.

--- program-generation/tree-reducer/deltaDebuggingReducer.js
+++ program-generation/tree-reducer/deltaDebuggingReducer.js
@@ -77,7 +77,6 @@
         }
     }
 
-
     /**
      * An input for the ddmin algorithm that uses text as input and splits it into char-tokens.
      */
@@ -140,12 +139,78 @@
         }
     }
 
+    /**
+     * An input for the ddmin algorithm that uses text as input and splits it into line-tokens.
+     */
+    class LineInput extends Input {
+        /**
+         *
+         * @param {string} text The text that comprises this input
+         * @param {Array.<string>} tokens optional tokens of the text.
+         *                                Auto-generated if omitted
+         * @param {Array.<number>} activeTokens optional list of indices of tokens
+         *                                      in the tokens list that are active. Set to all tokens if omitted.
+         */
+        constructor(text, tokens, activeTokens) {
+            if(tokens === undefined) {
+                tokens = text.split('\n');
+            }
+            if(activeTokens === undefined) {
+                activeTokens = [];
+                // Initially all tokens are active
+                for (var i = 0; i < tokens.length; i++) {
+                    activeTokens.push(i);
+                }
+            }
+            super(activeTokens);
+            this.text = text;
+            this.tokens = tokens;
+        }
+
+        /**
+         *
+         * @param  {number} num the number of the subset to obtain
+         * @return {LineInput} a new input object that has the same tokens, but only
+         * those of the specified subset are active
+         */
+        getSubset(num) {
+            return new LineInput(this.text, this.tokens, this.chunks[num]);
+        }
+
+        /**
+         *
+         * @param  {number} num the number of the complement to obtain
+         * @return {LineInput} a new input object that has the same tokens, but only
+         * those of the specified complement are active
+         */
+        getComplement(num) {
+            return new LineInput(this.text, this.tokens, super.getComplementChunks(num));
+        }
+
+        /**
+         * Obtains the code that results from putting all active tokens together.
+         * @return {string} the code that is obtained by concatenating all active
+         * tokens
+         */
+        get currentCode() {
+            var str = "";
+            for (var i = 0; i < this.activeTokens.length; i++) {
+                str = str + this.tokens[this.activeTokens[i]] + '\n';
+            }
+            return str;
+        }
+    }
+
     function ddminTree(tree, test) {
         return tree;
     }
 
     function ddminChar(text, test) {
         return ddmin(new TextInput(text), test).currentCode;
+    }
+
+    function ddminLine(text, test) {
+        return ddmin(new LineInput(text), test).currentCode;
     }
 
     // A map that serves as a cache for ddmin
@@ -249,4 +314,5 @@
 
     exports.ddminTree = ddminTree;
     exports.ddminChar = ddminChar;
+    exports.ddminLine = ddminLine;
 })();
--- program-generation/tree-reducer/input/test.js
+++ program-generation/tree-reducer/input/test.js
@@ -2,7 +2,9 @@ var x = 23;
 var y = 5;
 var z = [2,5,"NaN"];
 function foo() {
+	z.push(undefined);
 	while(x != y) {
+		var sum = x + y;
 		y++;
 	}
 	return z[2].fun();
