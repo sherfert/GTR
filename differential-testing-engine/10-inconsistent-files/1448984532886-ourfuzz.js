97c76b7c18b01be72de03e252f826e2c0a515779
Jibesh Patra <jibesh.patra@gmail.com>
2016-05-18 14:24:34 +0200

    Adding the inconsistent files

---
+++ differential-testing-engine/10-inconsistent-files/1448984532886-ourfuzz.js
@@ -0,0 +1,6 @@
+location = {
+    '\'isLessThanOrEqualTo\'': '\'\'',
+    23: function (a, src) {
+        patch.b = this.location.files(arguments.location(patch), '0', this, '\'submit\'').location;
+    }
+};
---
+++ differential-testing-engine/10-inconsistent-files/1449080666590-ourfuzz.js
@@ -0,0 +1,1 @@
+var winHei = event.newReadRef;
