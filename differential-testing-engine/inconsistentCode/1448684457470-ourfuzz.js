7064d065cb94037eb2e4c53e0aa75c55123451b1
Satia Herfert <satiaherfert@gmx.de>
2016-06-01 08:31:29 +0200

    Inconsistent files for the web worker version.

---
+++ differential-testing-engine/inconsistentCode/1448684457470-ourfuzz.js
@@ -0,0 +1,416 @@
+e ? e.a.m.r + 2 : !o({});
+function o(h) {
+    S = this.e - o + o + db.e(N);
+    _ = event.e.o;
+}
+this.o._ua = N[Qd]();
+var Kc = o.s(d, a, this, 'start Delay PrdynXml :: ', l.DcsInit(this, 'id'));
+var e = function () {
+    h[o](o, a);
+    W('input', o, 'type');
+};
+function Kc(q, e, db, p) {
+    for (s in k) {
+        var t = typeof 4 + '\';});\n';
+        if (node() || []) {
+            for (value in g.h)
+                l = this[1];
+            if (c.Kc(b, o)) {
+                var e = e.m.k(x), b = q;
+                l.q = 'newSiteIconsSprite greenClockIcon';
+                if (void 1)
+                    for (var y = q[open]; (100 === e.ba) + nLimit + _setContainment('for').b; q++)
+                        name = h.value ? [] : 'boolean' === (y[ed]('wdgespcricinfo') + 'boss' ? !1 + p.c : g) ? (x.g = function Kc(j) {
+                            k = _setContainment.a;
+                        }, f) : {
+                            'renderType': function () {
+                                if (y * u.r)
+                                    return this.e();
+                                return h.n || _;
+                            },
+                            _eq: t.e
+                        };
+                try {
+                    h.e = j.h.a.y(S);
+                } catch (a) {
+                    function _(C) {
+                        if (!'0') {
+                            if ('Not configuring conditionally-activated experiment (not enabled): ' === 4) {
+                                var n = f(j().e.get('.smart-banner')).o('m');
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        _.q = e[1].d = c.u(p);
+    }
+}
+this.c += this.q = a.c.S();
+b.value(e);
+h.i('data-' + q.value._(function (u) {
+    var y = '/[\\t\\r\\n\\f]/g';
+    a.C('null', E.type);
+}));
+this.value.a = '0';
+var q = p['0'](q);
+function e(isRel) {
+    d.q = g.p;
+}
+o.p = _.yb;
+s[1](C.W(this, e('""')));
+if (f.C) {
+    c = p || y(a(function (C) {
+        var e = function (e) {
+            if (e.a) {
+                g.e.y('null');
+                this.y(this, 'html');
+                return 'false';
+                s.u.d(function (q, p) {
+                    var F;
+                }) && (q = q || d.a(e <= e || k[j]));
+            }
+        };
+    }).TypeError.s, 'value');
+}
+f.value(d[b]);
+e = 64;
+p = this.n;
+u.a.a();
+k(i.q ? name.q(this.f).value : '\'\'');
+name(h());
+var k = !1;
+j(d);
+e = true;
+function x() {
+    if (this[q] && k.E.c(c.q(q.h), value)) {
+        var y = k(e, 403), i;
+    }
+    this.g(d.q, E);
+    h.C = 'nocontent';
+    var e = c.value;
+}
+y = d;
+this.a('target-').e = f(d) ? '0' : -'0';
+function s(F) {
+    if ((e && this.name(k) || 'fx') && this.j) {
+        s.p = 'program';
+    } else {
+        for (var e = S || (value = a().o(k), g = j, E.q = x = function () {
+                if (!a) {
+                    e.p.e = this['0'][a](a);
+                }
+            }, q[e]('\'.ui-button-text\'')); e;)
+            s.e = s.m();
+        if (h > 1) {
+            var q = e = function () {
+                '0' + 0.01 && (E == d) + '\'-"]\'';
+            };
+        }
+        a.value = '&c=' == this.g('self', (_['0'](e) + s + '\' \'' && !'0').e).e;
+        if (this._ || q.k || x.value('\'/stylesheets/external/sevenone_ad.css\'', c, q)) {
+            var args = [
+                    {
+                        e: 'Clear',
+                        f: q,
+                        y: function (f) {
+                            this.y(E);
+                            ga.f = k.e('0', '\'\'')[k];
+                            this[q] = function (e) {
+                                (j.yb(f) || e._) && k;
+                            };
+                            e = typeof 1 < '""' ? a.k = q : e;
+                        },
+                        q: 131,
+                        name: {
+                            s: true,
+                            d: function () {
+                                value[d] || this.name;
+                            },
+                            m: {
+                                a: function (qry) {
+                                    for (value in a.i)
+                                        e.e = !1;
+                                    function e() {
+                                        if (!name.p.name === 9)
+                                            return 1 + ('null' + 1) === !'0';
+                                        return a.value;
+                                        q('#comments .comments-action');
+                                        var j = a(e), a = E.value(), d = {
+                                                c: typeof ('_' + name && e + k),
+                                                'TXT1': [q]
+                                            }, c = e(this, this['0'][p]), d = i.TypeError(e.a, y, 'zoom overlay'), c, e = [
+                                                e,
+                                                '\'AdsPageTypes\'',
+                                                'singapore'
+                                            ], d = this;
+                                        a = '0';
+                                    }
+                                    q.d = e;
+                                    return j('iframe');
+                                    !'0' && (t && (name[o] = []));
+                                }
+                            },
+                            opts: 40,
+                            e: q
+                        },
+                        'headline': function (_) {
+                            return value.q(function (e, q, g) {
+                                a.k = i;
+                                var _ = q.a('Posts.Compartir');
+                                o = {
+                                    a: 'mouseout',
+                                    opts: {
+                                        k: o,
+                                        name: m.e
+                                    },
+                                    y: C
+                                };
+                                return;
+                                if (!(name(p) && 'Bad hint' + !1 instanceof '+dsts+\'/\'+y);fl=new Date(\'11/\'+dste+\'/\'+y);cd=new Date();'))
+                                    isRel.yb.d.e(options.E(c, '\'change\'', 8 != '\'undefined\'')).h = c;
+                                function value() {
+                                    return f(e, value.E(u.p).q(name, a('show', a))).F(isRel[h], function () {
+                                        return j;
+                                        if (p.c && s.k('prop')) {
+                                            this.q().e('false', a);
+                                        }
+                                    }, q[Array], TypeError);
+                                    value(_);
+                                }
+                            });
+                            e.j(this, p)[1]([
+                                'external/sha1',
+                                2.3072
+                            ], yb, 'P.Module');
+                            var e = (g || (p = i)) !== nLimit, f = -s.value, e = name.b, e = e.k({
+                                    g: [],
+                                    'name': function () {
+                                        for (var k, e = h.x(206); s(a).o + '0'; o--);
+                                    }
+                                });
+                        },
+                        e: this['0'],
+                        j: d,
+                        p: 'JP\xA5',
+                        d: function (isRel) {
+                            value(name);
+                            o.k.g(this.x.e('<img/>', function (db) {
+                                this.i = a > C;
+                            }).TypeError || 'null');
+                        },
+                        o: this.e(q[4]('code'), f),
+                        'Index': 'd5c40d574da6de33a6e96a105c53778f5a1782e262f399616898aaa0288fb751'
+                    },
+                    '""',
+                    '3020006584834785',
+                    'jQuery',
+                    '\'index/animation-strips/hero-intro-bg.jpg\'',
+                    4815.86,
+                    1.0745,
+                    s,
+                    'Sat'
+                ], E = p(), y = !this.k.o, g = 'map-overlay-rendered';
+            var j = a(c), q = new s._cn('0'), _ = p.a(k), opts = function (W) {
+                    s.Array();
+                }, k = !name, f = i.o, k = a.q(j), n, e = {
+                    22: 'ahxm6i911qnf9ar420ets8iho',
+                    'BlueGrassChannel': k.j.f(e, 10, function d(i) {
+                        k.o(s)._ = function () {
+                            m = a;
+                            j = e.p.b;
+                            q(s.C).e = g.c.x.e(f.m('\'AdsHelpTrayUIActions\'').c);
+                        };
+                    }).j
+                }, h = 'null', e = C(a.s), u = this, a = [], k = s, e = y.g('readOnly', a);
+        } else if (c && s(':hidden', q, 'P.genConst') && '""') {
+            a[a.e][value.d]();
+            var a;
+            var a = p[d].d('=', e), d = k[e](n(h).name.a().s.d.s.b)['default'], q = k.m = name.a(name.E), T = '0', q = e.value('""').q, k = c.m, g = m.c.a([
+                    {},
+                    '""'
+                ]), C, p, b = k.c(c, e.k, typeof 1);
+            e.value = p.y.s.e(500);
+        }
+        for (; (c.e.S >> y) + d;) {
+            var e = 'searchSuggestions', e = function (p, i, m, k, c) {
+                    if (o) {
+                        for (g in type)
+                            _setContainment[p] = function (o) {
+                                return e.e;
+                                y += e;
+                            };
+                        return a;
+                    }
+                }, yb = new nLimit(), g = '/<body[^>]*>\\s*([\\s\\S]+)\\s*<\\/body>/im';
+            function f() {
+                var s = m(a, n), name = E(g).k.e('#comments li.nested-reply');
+                var k, q, a = _ == C.k(p);
+                F.nLimit.t || this.e && (k.e().f = a.k('\'AdsConversionPixelStatusToText\'', l).o);
+                t('frm');
+                _.s(o.E.e(c));
+            }
+            if (e.p) {
+                var q;
+            }
+        }
+    }
+}
+j('.ifrm');
+p.k.e();
+type.q.e = q;
+this.ra('/grp=[0-9]*/', { f: 'watch' }, j);
+p = W;
+if (h.a > 1 | E && e === TypeError.c['\'facebookLogin\''][a])
+    c.e(k.q);
+if (c || 'border' >= m.m.e !== '0')
+    for (l.a = typeof 1; e !== k !== s.m[get] || '""'; j++) {
+        if (q.y.e(k, e)) {
+            y.d = s.e;
+        }
+        p.e = this.e(d[value](p, 'show'), name, q.i).e.E('\'g\'', e);
+        var o = e._;
+        if (yb.p.o && 32 == e) {
+            s = e.nLimit('null', !_.q);
+            e.g(TypeError.j(-!'0', f));
+            function g(a, h) {
+                var yb = t.k(847, q);
+            }
+        } else if (p[p](g[value]).S + o) {
+            j = g;
+            if (k.opts) {
+                this.k('href', s.k);
+            } else {
+                p = d;
+            }
+        }
+        this._ += g[q] == a;
+        value[c].e.b = [];
+        e.i = {
+            k: 'Numer telefonu',
+            b: function (y) {
+                e.a.E(9, 76, j(s, a.m(e.d % g && g[p[o]][_eq](e) && s !== k.p)));
+            },
+            x: '<li><a href=\'#{href}\'><span>#{label}</span></a></li>',
+            e: function (e) {
+                c = '0';
+                return DcsInit.Db(h, c, nLimit, W, '#cred_cancel_button');
+            },
+            s: function (s) {
+                if (k)
+                    for (yb = e.value(l).clearQueue().k.a().t(); 'production' - q;)
+                        m++;
+                return;
+            },
+            _: function () {
+                h.a = function (b, q) {
+                    for (var e in q)
+                        e.F(c + q.k[e], '\'name\'');
+                };
+                if (k.a.k && e()) {
+                    g = (!e.t || this.e || m('thumb', 268, a)).e;
+                }
+            }
+        };
+        s['0']('precision').a('""', a.value.s, 'classid');
+        'use strict';
+        function e() {
+            if (delete 1)
+                o.t('sc_off.src');
+        }
+        value || g.k;
+    }
+else if (this.p)
+    if (l.c.a(this)) {
+        a[f](k);
+        if (e / 'false') {
+            s._isIE(a);
+        }
+    }
+else if ('function' * d[g] + 'false' || (this.b.o[q] = {
+        _: function (g) {
+            function j() {
+                return h.q(this.a, E).o();
+                return !p;
+            }
+        },
+        o: function (s) {
+            return a._;
+        }
+    }))
+    for (; (g.d = 'XMLHttpRequest') === c; j[i] = e._v) {
+        var e = g.W()[k](this, i.k), i = s.p(e(',', o && d.j !== '0', function (f, g, c) {
+                return f[f++].k - '0';
+                s.t(function (u) {
+                    o = this.o.name;
+                });
+            }), name)._(j), c = {}, name, q = e.e('isObjEmpty:True'), TypeError = 'null';
+        u.y = d.f.q + !u(a(e, y).t.e).p ? a : 'prev';
+        d = { c: [e.e] };
+        '?date=' + u && p[c]();
+        this.a.a = E.a;
+        f = C = function () {
+            var isRel;
+            var h = o('stroke');
+        };
+        var n, c, d = new e.y(q.p), name = {}, c = k && (o += g);
+    }
+b(this);
+try {
+    yb = e = e || (o = a.p(a));
+    s.a = E;
+    var y = b(f);
+    var s = this.p;
+    a + '=' && a.k;
+    p.p();
+    this.a('/\\\\\\./g');
+} catch (g) {
+    var e = this, i = o();
+    function o(d) {
+        this.o = new f();
+        a.h(g.q);
+        this.p(k);
+        var g = q.f(!'0').l + e && name !== k, b = {
+                e: {
+                    n: function (m) {
+                        if (e)
+                            return b[k];
+                        var e = this.c('attPersistantLocalization'), e = this.q(f);
+                    }
+                }
+            };
+        if (q.s != p == name.k) {
+            nLimit._ = e;
+        }
+    }
+    var opts = h.n;
+    if (f.c + 'static') {
+        function g(e) {
+            c('widget');
+        }
+        k.yb('.cd-headline', this[j]);
+        if (g.k()) {
+            k.value = e[q];
+            d.opts(function () {
+                g.q.a = (u === u) + c;
+            });
+        }
+    }
+}
+var j;
+var value = a.g(_, name), j;
+this.s('\'phone-show\'', 'lazy-load');
+e[e](',');
+j = g;
+s.q = name(!e);
+var u = '0';
+p = e.a('dp1');
+_ = '0';
+g('\'hidden\'').q.E(t, f);
+var db = this[Kc]();
+a.e.a('timing').t('P.Module');
+function name(a, e) {
+    function e(e, m, c, a, e, a) {
+        e = s;
+    }
+}
